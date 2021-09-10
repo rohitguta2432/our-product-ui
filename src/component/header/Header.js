39c09dbeb0f136df3df62b6b3f9068ee45287a12
rohit <rohitgupta2432@gmail.com>
2021-09-10 23:20:42 +0530

    initial commit

---
+++ src/component/header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <>
+            <div className="main_cointainer_home">
+                Logo Name
+                <ul className="item_list">
+                    <li className="about_item1">
+                        About Us
+                    </li>
+                    <li className="contact_item2">
+                        Contact Us
+                    </li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Header
---
+++ src/component/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../footer/Footer'
+import Header from '../header/Header'
+
+const Home = () => {
+    return (
+        <>
+            <Header/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Home
